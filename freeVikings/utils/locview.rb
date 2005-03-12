894fd127305f1a1dbb95d63de20274815e2e0581
igneus <igneus>
2005-03-12 21:34:48 +0000

    Upravy prohlizece. Rozsiren o par funkci.

--- freeVikings/utils/locview.rb
+++ freeVikings/utils/locview.rb
@@ -6,34 +6,44 @@
 $:.push(File.expand_path('..'))
 
 require 'RUDL'
+
 require 'map'
 require 'location'
 require 'locationloadstrategy'
-
-require 'sprite'
 
 module FreeVikings
   GFX_DIR = File.expand_path 'gfx'
 end
 
+class NonScriptLocationLoadStrategy < FreeVikings::XMLLocationLoadStrategy
+  def load_monsters(location)
+  end
+end
+
 class Browser
 
   VIEW_MOVE = 2
+  TITLE = 'freeVikings Location Browser'
 
   include FreeVikings
   include RUDL::Constant
 
   def initialize
     puts 'Creating a new location browser.'
-    @win = RUDL::DisplaySurface.new [300,300]
+    open_win
     @loc = nil
   end
+
+  # loads a specified location file
 	
   def view(location_path)
-    @loc = Location.new(XMLLocationLoadStrategy.new(location_path))
-    @view_center = [150,150]
-    @center_move = [0,0]
+    @loc = Location.new(NonScriptLocationLoadStrategy.new(location_path))
+    self.win_caption = TITLE + ': ' + File.basename(location_path)
+    @view_center = [150,150] # stred zobrazovane oblasti mapy
+    @center_move = [0,0] # rychlost pohybu h i v smeru
   end
+
+  # Updates view
 	
   def update
     @view_center[0] += @center_move[0]
@@ -43,7 +53,15 @@ class Browser
     @win.flip
   end
 	
+  # Sets the display window's title
+
+  def win_caption=(caption)
+    @win.set_caption caption
+  end
+
   private
+
+  # Serves event
 	
   def serve_event(event)
     if event.is_a? RUDL::KeyDownEvent then
@@ -59,6 +77,14 @@ class Browser
 	@center_move[0] = - VIEW_MOVE
       when K_RIGHT
 	@center_move[0] = VIEW_MOVE
+      when K_PAGEUP
+	@view_center[1] -= @win.h
+      when K_PAGEDOWN
+	@view_center[1] += @win.h
+      when K_HOME
+	@view_center[0] = @win.w / 2
+      when K_END
+	@view_center[0] = @loc.background.w - (@win.w / 2)
       end
     end
     if event.is_a? RUDL::KeyUpEvent then
@@ -69,6 +95,16 @@ class Browser
 	@center_move[0] = 0
       end
     end
+    if event.is_a? RUDL::QuitEvent then
+      exit
+    end
+  end
+
+  # Opens the display window
+
+  def open_win
+    @win = RUDL::DisplaySurface.new [300,300]
+    self.win_caption = TITLE
   end
 end
 
@@ -80,5 +116,3 @@ puts 'Enjoy browsing...'
 loop {
   b.update
 }
-
-exit
