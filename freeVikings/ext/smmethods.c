48b50943323682370bb4e6ace15a18c887386743
igneus <igneus>
2005-04-09 10:38:50 +0000

    Zrychlovaci snahy zahajeny zacatkem praci na prepsani SpriteManageru do C.

---
+++ freeVikings/ext/smmethods.c
@@ -0,0 +1,17 @@
+/*
+smmethods.c
+
+igneus 9.4.2005
+A part of SpriteManager extension module.
+
+This file contains all the procedures which are called whenever
+Ruby code calls some of the FreeVikings::Extensions::SpriteManager methods.
+*/
+
+#include "ruby.h"
+#include "spritemanager.h"
+
+VALUE sm_initialize(VALUE self_obj, VALUE arguments)
+{
+
+}
---
+++ freeVikings/ext/spritemanager.h
@@ -0,0 +1,12 @@
+/*
+spritemanager.h
+
+igneus 9.4.2005
+SpriteManager extension module global definitions.
+*/
+
+/* Structure for SpriteManager objects internal data storage */
+
+
+/* SpriteManager instance methods routines: */
+VALUE sm_initialize(VALUE self_obj, VALUE arguments);
