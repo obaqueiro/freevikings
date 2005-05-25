d7964d02ecbdea24b61f738d8dd93b75c087b99d
igneus <igneus>
2005-05-25 23:54:58 +0000

    Soubor, ktery jsem minule zapomnel pridat. Uchovava pohybovy stav vikinga ve vertikalnim smeru.

---
+++ freeVikings/verticalstate.rb
@@ -0,0 +1,15 @@
+# verticalstate.rb
+# igneus 23.5.2005
+
+# Abstraktni trida VerticalState a jeji podtridy.
+# Soucast noveho pojeti myslenky VikingState.
+
+module FreeVikings
+
+  class VerticalState
+
+    def velocity
+      0
+    end
+  end # class VerticalState
+end # module FreeVikings
