57b96698db985cc07d12b86298d6d40671795e6d
igneus <igneus>
2005-06-16 07:50:30 +0000

    Jak uvedeno drive, zahajil jsem vyvoj aktivnich objektu.

---
+++ freeVikings/activeobject.rb
@@ -0,0 +1,12 @@
+# activeobject.rb
+# igneus 16.6.2005
+
+=begin
+= ActiveObject
+=end
+
+module FreeVikings
+
+  class ActiveObject
+  end # class ActiveObject
+end # module FreeVikings
---
+++ freeVikings/tests/testactiveobject.rb
@@ -0,0 +1,28 @@
+# testactiveobject.rb
+# igneus 16.6.2005
+
+# ActiveObject is an entity of the freeVikings game world, which
+# doesn't change it's state or position from it's own initiative
+# (it means ActiveObjects don't need to be updated regularly).
+# It's only informed when something happens to it (a player comes and
+# presses the 'activate' or 'unactivate' key or some weapon hurts the
+# ActiveObject).
+
+require 'test/unit'
+
+require 'activeobject.rb'
+
+class TestActiveObject < Test::Unit::TestCase
+
+  def setup
+    @object = FreeVikings::ActiveObject.new
+  end
+
+  def testActiveObjectClassExists
+    assert_kind_of Class, FreeVikings::ActiveObject, "The class 'ActiveObject' must exist at first."
+  end
+
+  def testMethodActivateExists
+    assert_respond_to @object, :activate, "ActiveObject should have a method called 'activate' which is called when the player pushes the 'S' key over it."
+  end
+end
