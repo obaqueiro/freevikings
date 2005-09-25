a15410c16ba7bdcb4d8639c8c7602eee98aa63ff
igneus <igneus>
2005-09-25 14:47:09 +0000

    Rozchodil jsem rozhovory.
    Oranzova hlava povida. Rozhovor se nacita z datoveho souboru
    small_talk.yaml.

--- freeVikings/locs/TestsCampaign/MonsterTestsLevelSet/OrangeHead/orangehead_test_script.rb
+++ freeVikings/locs/TestsCampaign/MonsterTestsLevelSet/OrangeHead/orangehead_test_script.rb
@@ -2,7 +2,11 @@
 # igneus 24.9.2005
 
 require 'monsters/orangehead.rb'
+require 'talk.rb'
 
 FLOOR = 440
 
-MONSTERS << OrangeHead.new([200, FLOOR - OrangeHead::HEIGHT])
+talk_source = File.open(File.dirname(__FILE__) + '/small_talk.yaml')
+
+MONSTERS << OrangeHead.new([200, FLOOR - OrangeHead::HEIGHT],
+                           Talk.new(talk_source))
