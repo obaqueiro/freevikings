b1ae10f6ccf17a3e24bfab5a9dba23f16046d69a
igneus <igneus>
2005-10-06 22:04:42 +0000

    Pridany dva vytahy (hura, po vice nez pul roce vyvoje jsem se dokopal k implementaci vytahu!) a prepinac. Lokace Meadow oziva.

--- freeVikings/locs/DefaultCampaign/ForestLevelSet/Meadow/meadow_script.rb
+++ freeVikings/locs/DefaultCampaign/ForestLevelSet/Meadow/meadow_script.rb
@@ -3,7 +3,10 @@
 
 # location script for location Meadow
 
+require 'switch.rb'
+
 require 'monsters/apex.rb'
+require 'monsters/transporterbridge.rb'
 
 TS = 40
 
@@ -18,3 +21,22 @@ end
 
 # some more apexes under the stairs:
 LOCATION.add_sprite(ApexRow.new([19*TS,24*TS], 4, LOCATION.theme))
+
+# The left 'lift' bridge:
+left_bridge = TransporterBridge.new(TS*1, [TS*4,TS*21], LOCATION.theme)
+LOCATION.add_sprite left_bridge
+
+# and it's switch:
+blueroom_switch = Switch.new([40*TS, 23*TS], LOCATION.theme, false,
+                             Proc.new {|state|
+                               if state then
+                                 left_bridge.move_down
+                               end
+                             })
+LOCATION.activeobjectmanager.add blueroom_switch
+
+# the right 'lift' bridge:
+right_bridge = TransporterBridge.new(TS*26, [TS * 4, TS * 12, TS * 25],
+                                     LOCATION.theme)
+right_bridge.dest = 1
+LOCATION.add_sprite right_bridge
