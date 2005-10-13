217338a3fddaa946b1aec592dd496c7066bfcacc
igneus <igneus>
2005-10-13 19:11:58 +0000

    Pridana testovaci lokace pro sneka.

---
+++ freeVikings/locs/TestsCampaign/MonsterTestsLevelSet/Snail/snail_script.rb
@@ -0,0 +1,10 @@
+# snail_script.rb
+# igneus 13.10.2005
+
+# First test of monster class Snail.
+
+require 'monsters/snail.rb'
+
+TS = 40
+
+LOCATION.add_sprite Snail.new([10*TS, 11*TS - Snail::HEIGHT])
