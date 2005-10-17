576b16f78236198682a0d5310b91d0b5c8346c37
igneus <igneus>
2005-10-17 19:37:29 +0000

    Snail: Neuspesne snahy o rozchozeni. (Nechce plivat jak by mel.) Shooter: Vetsina funkcionality presunuta do modulu Shooting.

--- freeVikings/monsters/snail.rb
+++ freeVikings/monsters/snail.rb
@@ -1,6 +1,12 @@
 # snail.rb
 # igneus 13.10.2005
 
+# !!! FIXME:
+# !!! THIS STRONGLY NEEDS TO BE SOLVED!
+# !!! DOESN'T DO WHAT IT SHOULD!
+# !!! I WANT THE SNAIL TO GO SOME WAY, STOP, SHOOT, WAIT UNTIL THE SHOT
+# !!! IS DESTROYED AND THAN GO AGAIN. BUT IT DOESN'T DO THIS!
+
 =begin
 = NAME
 Snail
@@ -19,6 +25,8 @@ require 'sophisticatedspritestate.rb'
 require 'sophisticatedspritemixins.rb'
 require 'monster.rb'
 require 'monstermixins.rb'
+require 'shooting.rb'
+require 'hero.rb'
 
 module FreeVikings
 
@@ -34,16 +42,22 @@ MonsterMixins::ShieldSensitive
     include SophisticatedSpriteMixins::Walking
     include Monster
     include MonsterMixins::ShieldSensitive
+    include Shooting
 
     WIDTH = 90
     HEIGHT = 70
 
     BASE_VELOCITY = 26
 
+    SHOT_VELOCITY = 65
+
+    GO_BETWEEN_SHOOTING = 150
+
     def initialize(position)
       super(position)
       @state = SophisticatedSpriteState.new
       @energy = 2
+      @way_length = 0
       move_right
     end
 
@@ -57,7 +71,9 @@ MonsterMixins::ShieldSensitive
         turn
       end
 
-      @rect.left += velocity_horiz * @location.ticker.delta
+      way_change = velocity_horiz * @location.ticker.delta
+      @rect.left += way_change
+      update_way way_change
     end
 
     def init_images
@@ -72,17 +88,63 @@ MonsterMixins::ShieldSensitive
 
     private
 
+    def update_way(way_change)
+      @way_length += way_change.abs
+
+      if @way_length >= GO_BETWEEN_SHOOTING
+        @way_length = 0
+        stop_and_shoot
+      end
+    end
+
+    def stop_and_shoot
+      @state.stop
+      @last_spittle = shoot
+    end
+
     # Turns back and starts moving.
     def turn
+      puts 'turning back'
+      @state.move_back
+    end
+
+    def go_on
       if @state.right? then
-        move_left
-      else
         move_right
+      else
+        move_left
       end
+    end
+
+    def shoot
+      pos = velocity_horiz > 0 ? [@rect.left + WIDTH, @rect.top+10] : 
+            [@rect.left - PoisonousSpittle::WIDTH, @rect.top + 10]
+      veloc = velocity_horiz > 0 ? SHOT_VELOCITY : 
+              -SHOT_VELOCITY
+      spittle = PoisonousSpittle.new(pos, veloc)
+      @location.add_sprite(spittle)
+      return spittle
     end
 
     def velocity_horiz
       BASE_VELOCITY * @state.velocity_horiz
     end
+
+
+    class PoisonousSpittle < Shot
+
+      def initialize(position, velocity)
+        super(position, velocity)
+        @hunted_type = Hero
+      end
+
+      def init_images
+        if @velocity < 0 then
+          @image = Image.load 'snail_spittle_left.tga'
+        else
+          @image = Image.load 'snail_spittle_right.tga'
+        end
+      end
+    end # class PoisonousSpittle
   end # class Snail
 end # module FreeVikings
