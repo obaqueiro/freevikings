35c38e81ec882452563eb6b680e2aece5414ca47
igneus <igneus>
2005-10-18 21:31:40 +0000

    Pridani hlemyzdi a jedno jablicko. Tim lokaci povazuji za dokoncenou.

--- freeVikings/locs/DefaultCampaign/ForestLevelSet/Meadow/meadow_script.rb
+++ freeVikings/locs/DefaultCampaign/ForestLevelSet/Meadow/meadow_script.rb
@@ -8,11 +8,13 @@ require 'switch.rb'
 require 'door.rb'
 require 'lock.rb'
 require 'key.rb'
+require 'apple.rb'
 
 require 'monsters/apex.rb'
 require 'monsters/transporterbridge.rb'
 require 'monsters/flyingplatform.rb'
 require 'monsters/arrowshooter.rb'
+require 'monsters/snail.rb'
 
 TS = LOCATION.map.class::TILE_SIZE
 
@@ -40,12 +42,19 @@ end
 apexes = ApexRow.new([19*TS,BLUEROOM_FLOOR - TS], 4, LOCATION.theme)
 LOCATION.add_sprite(apexes)
 
+# Snail in the left corridor:
+snail = Snail.new([60, LEFT_CORRIDOR_FLOOR - Snail::HEIGHT])
+LOCATION.add_sprite snail
+
 # The left 'lift' bridge:
 left_bridge = TransporterBridge.new(TS*1, 
                                     [HIGH_PLATFORMS_FLOOR, 
                                      LEFT_CORRIDOR_FLOOR], 
                                     LOCATION.theme)
 LOCATION.add_sprite left_bridge
+
+# an apple:
+LOCATION.add_item Apple.new([2*TS+5, HIGH_PLATFORMS_FLOOR - 2*TS])
 
 # and it's switch:
 blueroom_switch = Switch.new([40*TS, BLUEROOM_FLOOR - 2 * TS], 
@@ -102,6 +111,10 @@ LOCATION.add_sprite flying_plat
 # the door to EXIT
 exit_door = Door.new([49*TS, BLUEROOM_FLOOR-3*TS], true)
 LOCATION.map.static_objects.add exit_door
+
+# the snail which waits at the EXIT to say the vikings goodbye
+exit_snail = Snail.new([53*TS, BLUEROOM_FLOOR - Snail::HEIGHT])
+LOCATION.add_sprite exit_snail
 
 # lock which opens the door
 exit_lock = Lock.new([47.5*TS, BLUEROOM_FLOOR-2*TS],
