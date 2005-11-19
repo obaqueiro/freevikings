17b8d56c344223fae7a6364ddb725195b9f0e23e
igneus <igneus>
2005-11-19 22:05:55 +0000

    Konec mixinu Platform, misto toho se budou pouzivat objekty, ktere maji v Location vice roli (Bridge je Sprite a StaticObject)

--- freeVikings/monsters/bridge.rb
+++ freeVikings/monsters/bridge.rb
@@ -3,17 +3,27 @@
 
 # A bridge for the vikings.
 
-require 'platform'
+#require 'platform'
 require 'sprite.rb'
+require 'staticobject.rb'
 
 module FreeVikings
 
   class Bridge < Sprite
 
-    include Platform
+    include StaticObject
+
+    def solid
+      true
+    end
 
     def init_images
       @image = Image.load 'yellow_map/small_bridge.tga'
+    end
+
+    def register_in(location)
+      location.add_sprite self
+      location.add_static_object self
     end
   end # class Bridge
 end # module FreeVikings
