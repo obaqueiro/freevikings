989b70ef20890b817f5a2f9f25f68d57fadd506f
igneus <igneus>
2006-08-01 16:35:46 +0000

    Level zase o neco malo blizsi dokonceni, tentokrat pridany dvere a vypinace od vytahu posunuty na lepsi mista

--- freeVikings/locs/DefaultCampaign/TechnoLevelSet/StairsToHell/stairs_script.rb
+++ freeVikings/locs/DefaultCampaign/TechnoLevelSet/StairsToHell/stairs_script.rb
@@ -5,8 +5,12 @@ require 'monsters/lift'
 require 'helpbutton'
 require 'monsters/animatedapex'
 require 'switch'
+require 'door'
+require 'killtoy'
 
 ###### STAIRS HALL
+# Erik must move lifts so that all the vikings can go down on them as
+# on stairs.
 
 # lifts which Erik must move so that they can be used as stairs down
 bridge_y = []
@@ -36,7 +40,21 @@ LOCATION << HelpButton.new([1520, 80],
 # lift up to the lift-key hall
 LOCATION << (interhall_lift = Lift.new(31*40, [35*40, 7*40], LOCATION.theme))
 # switches to call the lift:
-LOCATION << Switch.new([32*40,5*40], LOCATION.theme, false, 
+LOCATION << Switch.new([30*40+10,5*40], LOCATION.theme, false, 
                        Proc.new {interhall_lift.move_up})
-LOCATION << Switch.new([32*40, 33*40], LOCATION.theme, false, 
+LOCATION << Switch.new([34*40+10, 33*40], LOCATION.theme, false, 
                        Proc.new {interhall_lift.move_down})
+
+# bonus on a hidden place
+LOCATION << Killtoy.new([45*40, 3*40])
+
+###### LIFT-KEY HALL
+# Erik must move the special lifts so that they are on the emphasized places.
+# The door to the next place is openned then.
+
+# doors between the rooms with monsters
+LOCATION << Door.new([14*40,32*40])
+LOCATION << Door.new([22*40,32*40])
+
+# door at the left top corner of the hall
+LOCATION << Door.new([8*40,2*40])
