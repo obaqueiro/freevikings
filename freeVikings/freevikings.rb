5652df307776afac553a1d53524ead12de908ddb
igneus_cz <igneus_cz>
2008-08-23 13:16:18 +0000

    Added changelog (which keeps track of all changes, not only those which I planned before, as TODO has done until now).
    game.rb RD -> RDoc documentation
    
    Improved help mechanism - help about commandline option is generated and thus it's always fresh without a need to update HELP file whenever changing the options.

--- freeVikings/freevikings.rb
+++ freeVikings/freevikings.rb
@@ -6,44 +6,139 @@
 # FreeVikings project is trying to clone the brilliant
 # Lost Vikings game.
 
+# !!! more require-statements (SchwerEngine, Log4r) are at the end of this file
 require 'fvdef.rb' # the FreeVikings module definition. Must be included first!
-
-require 'schwerengine/schwerengine.rb'
-SchwerEngine.init(SchwerEngine::DISABLE_LOG4R_SETUP)
-include SchwerEngine
-SchwerEngine.config = FreeVikings
-
-require 'initfv.rb' # Init class (just btw.: in Hebrew - which doesn't use syllables in the written text - 'Init' would probably be written the same as 'Anat', which is a goddess known from the Kenaan mythology)
-
 require 'getoptlong'
-require 'log4r'
-require 'log4rsetupload'
 
 include FreeVikings
+
+# constants used for text formatting
+LONG_OPT_W = 18
+SHORT_OPT_W = 5
+TERMINAL_W = 80
+TEXT_W = TERMINAL_W - (LONG_OPT_W + 1 + SHORT_OPT_W + 1)
 
 def print_help_and_exit
   File.open('HELP') do |f|
     f.each_line {|l| puts l}
   end
   puts
+
+  puts "OPTIONS\n\n"
+
+  short_options_summary = "-"
+  long_options_summary = ""
+  options_full = ""
+
+  # constant OPTIONS_DEF is defined below
+  OPTIONS_DEF.each do |o|
+    short_options_summary.concat o[1].slice(1,1)
+
+    long_options_summary.concat(" ["+o[0])
+    if o[2] == GetoptLong::REQUIRED_ARGUMENT then
+      long_options_summary.concat " "+o[4]
+    elsif o[2] == GetoptLong::OPTIONAL_ARGUMENT then
+      long_options_summary.concat " ["+o[4]+"]"
+    end
+    long_options_summary.concat "]"
+
+    # a bit complicated process of formatting of full options' description:
+    options_full.concat(o[0].ljust(LONG_OPT_W))
+    options_full.concat o[1].ljust(SHORT_OPT_W)+" "
+
+    l = o[3]
+    first_line = true
+    begin
+      if l.size <= TEXT_W then
+        l = l.ljust(TEXT_W)
+        l = l.rjust(TERMINAL_W-2) if !first_line
+        options_full.concat l+"\n"
+        l = nil
+      else
+        # This would lead to infinite loop if there was some option comment
+        # without spaces!
+
+        # find last space before end of screen:
+        i = 0
+        TEXT_W.downto(0) {|x|
+          if l[x].chr == " " then
+            i = x
+            break
+          end
+        }
+        # now divide the text into two...
+        s = l.slice(0, i).ljust(TEXT_W)
+        s = s.rjust(TERMINAL_W-2) if !first_line
+        options_full.concat s+"\n"
+        l = l.slice(i, l.size)
+        first_line = false
+      end
+    end while l
+    options_full.concat "\n"
+  end
+
+  puts "ruby freevikings.rb [#{short_options_summary}]#{long_options_summary}"
+  puts
+  puts options_full
+
   exit
 end
 
-options = GetoptLong.new(
-                         ["--profile", "-P", GetoptLong::NO_ARGUMENT],
-                         ["--ruby-warnings", "-w", GetoptLong::NO_ARGUMENT],
-                         ["--extensions", "-x", GetoptLong::NO_ARGUMENT],
-                         ["--fps",     "-F", GetoptLong::NO_ARGUMENT],
-                         ["--fullscreen", "-f", GetoptLong::NO_ARGUMENT],
-                         ["--help",    "-h", GetoptLong::NO_ARGUMENT],
-                         ["--levelsuite", "-l", GetoptLong::REQUIRED_ARGUMENT],
-                         ["--v-unit",  "-u", GetoptLong::REQUIRED_ARGUMENT],
-                         ["--delay",   "-d", GetoptLong::REQUIRED_ARGUMENT],
-                         ["--startpassword", "-s", GetoptLong::REQUIRED_ARGUMENT],
-                         ["--skip-menu", "-m", GetoptLong::NO_ARGUMENT],
-                         ["--skip-password", "-Z", GetoptLong::NO_ARGUMENT],
-                         ["--develmagic", "-D", GetoptLong::NO_ARGUMENT]
-)
+# Standard arguments for GetoptLong.new are Arrays of three members:
+# long option, short option and argument flag.
+# I added two more which are used to generate help:
+# fourth is short descriptin and fifth argument description (only at options
+# which receive argument).
+# GetoptLong wouldn't receive such an extended argument, so before
+# filling it all to GetoptLong, every Array is sliced.
+OPTIONS_DEF = [
+               ["--profile", "-P", GetoptLong::NO_ARGUMENT,
+                "Profiling (only for development)"],
+
+               ["--ruby-warnings", "-w", GetoptLong::NO_ARGUMENT,
+                "Show ruby warnings"],
+               
+               ["--extensions", "-x", GetoptLong::NO_ARGUMENT,
+                "DEPRECATED! Uses compiled C++ extensions instead of some of the critical classes. You must have built these extensions before you use this option."],
+               
+               ["--fps",     "-F", GetoptLong::NO_ARGUMENT,
+                "Prints frames per second on the game screen"],
+               
+               ["--fullscreen", "-f", GetoptLong::NO_ARGUMENT,
+                "Starts the game in the fullscreen mode"],
+               
+               ["--help",    "-h", GetoptLong::NO_ARGUMENT,
+                "Prints out this help and halts."],
+               
+               ["--levelsuite", "-l", GetoptLong::REQUIRED_ARGUMENT,
+                "Play levels from given levelsuite directory LEVELSITE_DIR instead of the default campaign.",
+                "LEVELSUITE_DIR"],
+
+               ["--v-unit",  "-u", GetoptLong::REQUIRED_ARGUMENT,
+                "Requires an argument - real number higher than 0 influencing speed of movement of vikings and monsters (higher than 1 increases the speed). If you want to speed-up the game a bit, try e.g. 3; high numbers will cause interesting errors.",
+                "NUMBER"],
+               
+               ["--delay",   "-d", GetoptLong::REQUIRED_ARGUMENT,
+                "Number of milliseconds to sleep in every iteration of the main loop (it is very useful if the game runs on your computer with fps 40 or even higher - try 10 milliseconds)",
+                "MILLISECONDS"],
+               
+               ["--startpassword", "-s", GetoptLong::REQUIRED_ARGUMENT,
+                "Jump to a level with password PASSWORD (you can see the password always at the beginning of every level)",
+                "PASSWORD"],
+               
+               ["--skip-menu", "-m", GetoptLong::NO_ARGUMENT,
+                "Skip menu"],
+               
+               ["--skip-password", "-Z", GetoptLong::NO_ARGUMENT,
+                "Don't display password at the beginning of level"]
+
+               #, ["--develmagic", "-D", GetoptLong::NO_ARGUMENT,
+               # ""]
+              ]
+
+# here is every argument-definition Array sliced to what GetoptLong wants;
+# everything is then given into GetoptLong
+options = GetoptLong.new(*(OPTIONS_DEF.collect {|o_array| o_array.slice(0,3)}))
 
 begin
   options.each do |option, argument|
@@ -86,9 +181,13 @@ begin
     end
   end # options.each block
 rescue GetoptLong::InvalidOption => ioex
+  # normally Log4r is loaded a few lines below, but here it is needed...
+  require 'log4r'
+  require 'log4rsetupload'
   Log4r::Logger['init log'].fatal "Option error: "+ioex.message
   puts
-  print_help_and_exit
+  puts "'freevikings.rb -h' for detailed description of commandline options"
+  exit
 end
 
 # This must be out of the block scope in which all the other
@@ -102,6 +201,20 @@ if OPTIONS['profile'] then
     Profiler__::print_profile(STDERR)
   }
 end
+
+# All the huge requirements are done here, after options are processed,
+# because it was terrible to type 'freevikings -h' (I can't remember that 
+# option!) and wait 10 seconds until SchwerEngine and Log4r were loaded
+require 'log4r'
+require 'log4rsetupload'
+
+require 'schwerengine/schwerengine.rb'
+SchwerEngine.init(SchwerEngine::DISABLE_LOG4R_SETUP)
+include SchwerEngine
+SchwerEngine.config = FreeVikings
+
+require 'initfv.rb' # Init class (just btw.: in Hebrew - which doesn't use syllables in the written text - 'Init' would probably be written the same as 'Anat', which is a goddess known from the Kenaan mythology)
+
 
 $:.concat FreeVikings::CODE_DIRS
 
--- freeVikings/game.rb
+++ freeVikings/game.rb
@@ -12,50 +12,46 @@ require 'bottompanel.rb'
 
 module FreeVikings
 
-=begin
-= Game
-All the stones which the house is built of are important. Classes are 
-such stones. (({Game})) is a roof over the house made of stones. Since 
-the roof is finished, you can live in the house. Roof makes the house 
-habitable.
-(({Game})) makes the game playable.
+  # All the stones which the house is built of are important. Classes are 
+  # such stones. Game is a roof over the house made of stones. Since 
+  # the roof is finished, you can live in the house. Roof makes the house 
+  # habitable.
+  # Game makes the game playable.
+  #
+  # One Game object rules the program all the time we play.
+  # It reacts on the events, updates the screen.
+  #
+  # From it's internals I will mention one attribute - @world.
+  # It's a World object and contains information about locations which 
+  # the vikings should explore and escape from. Maybe you expect another attribute 
+  # which contains the currently loaded location. It isn't here, it's a local 
+  # variable in method Game#game_loop.
+  #
+  # Then there is one more attribute. I don't want to talk much about it.
+  # It's a bit stinking attribute... It's name is @state. If you have ever 
+  # heard about the Design patterns, you understand. When I was designing 
+  # the Game class, I was really excited about the design patterns and 
+  # I had a very nice idea of using the State design pattern to implement
+  # the game menu etc. Now I think that idea isn't good any more and as soon as
+  # I have a good mood for it, I will make @state go away.
 
-One Game object rules the program all the time we play.
-It reacts on the events, updates the screen.
-
-From it's internals I will mention one attribute - ((|@world|)).
-It's a (({World})) object and contains information about locations which 
-the vikings should explore and escape from. Maybe you expect another attribute 
-which contains the currently loaded location. It isn't here, it's a local 
-variable in method ((<Game#game_loop>)).
-
-Then there is one more attribute. I don't want to talk much about it.
-It's a bit stinking attribute... It's name is ((|@state|)). If you have ever 
-heard about the Design patterns, you understand. When I was designing 
-the (({Game})) class, I was really excited about the design patterns and 
-I had a very nice idea of using the State design pattern to implement
-the game menu etc. Now I think that idea isn't good any more and as soon as
-I have a good mood for it, I will make ((|@state|)) go away.
-=end
   class Game
 
     include RUDL
     include RUDL::Constant
 
-=begin
-== Public instance methods
+    # == Public instance methods
+    #
+    # Argument window should be a RUDL::Surface (or RUDL::DisplaySurface).
+    # It is used as a main game screen.
+    # About the second argument, locations, will I write later.
+    # An initialization prepares the Game instance for use, but doesn't start 
+    # the game. It doesn't change the contents of the window window and doesn't
+    # receive any user events.
+    # It means you theoretically could create two Game objects, both working with
+    # the same window, and use them subsequently (or, if you were a devil, both
+    # at once in a multithreaded application). But I wouldn't do that...
 
---- Game.new(window, password='')
-Argument ((|window|)) should be a RUDL::Surface (or RUDL::DisplaySurface).
-It is used as a main game screen.
-About the second argument, ((|locations|)), will I write later.
-An initialization prepares the Game instance for use, but doesn't start 
-the game. It doesn't change the contents of the window ((|window|)) and doesn't
-receive any user events.
-It means you theoretically could create two Game objects, both working with
-the same window, and use them subsequently (or, if you were a devil, both
-at once in a multithreaded application). But I wouldn't do that...
-=end
     def initialize(window, startpassword='')
       @app_window = window
 
@@ -83,10 +79,10 @@ at once in a multithreaded application). But I wouldn't do that...
       # Surface used to build the "picture" of a part of map and game objects
       # which is then displayed on the screen
       @map_view = RUDL::Surface.new([WIN_WIDTH, WIN_HEIGHT - BottomPanel::HEIGHT])
-
+      
       # State of the game (GameState instance).
       @state = nil
-
+      
       @bottompanel = nil
 
       @give_up = nil
@@ -94,52 +90,31 @@ at once in a multithreaded application). But I wouldn't do that...
 
     public
 
-=begin
---- Game#app_window
-(({RUDL::DisplaySurface})) on which the game world is displayed.
-It's that one which was given as an argument to the constructor.
-=end
-
+    # RUDL::DisplaySurface on which the game world is displayed.
+    # It's that one which was given as an argument to the constructor.
     attr_reader :app_window
 
-=begin
---- Game#team
-Returns (({Team})) of vikings.
-=end
-
+    # Returns Team of vikings.
     attr_reader :team
 
-=begin
---- Game#bottompanel
-(({BottomPanel})) which displays the vikings' portraits, life statistics
-and inventories.
-=end
-
+    # BottomPanel which displays the vikings' portraits, life statistics
+    # and inventories.
     attr_reader :bottompanel
 
-=begin
---- Game#give_up_game
-This method is called by the GameState when a player presses
-the give up key (F6 by default). Causes location reloading.
-=end
+    # This method is called by the GameState when a player presses
+    # the give up key (F6 by default). Causes location reloading.
 
     def give_up_game
-	@give_up = true
+      @give_up = true
     end
 
-=begin
---- Game#exit_game
-Exits the game.
-=end
+    # Exits the game.
 
     def exit_game
       throw :return_to_menu
     end
 
-=begin
---- Game#pause
-Pauses all the (({Sprite}))s and switches into the inventory browsing mode.
-=end
+    # Pauses all the Sprites and switches into the inventory browsing mode.
 
     def pause
       @world.location.pause
@@ -158,47 +133,45 @@ Pauses all the (({Sprite}))s and switches into the inventory browsing mode.
       @state = DevelopmentMagicGameState.new self
     end
 
-=begin
---- Game#game_loop
-When this method is called, the real fun begins (well, I know freeVikings
-don't provide the real fun yet, but after a long startup procedure you
-could think about the simple game like about a fun).
-On the screen appears a strange place and three small vikings.
-Method 'game_loop' contains two nested loops.
-In the first one every iteration means one played location.
-A new iteration starts whenever a player finishes or gives up a location
-or if all the vikings are dead or if the remainder of them finishes the
-location.
-The nested loop updates all the sprites (heroes and their enemies)
-regularly and refreshes the screen.
-=end
+    # When this method is called, the real fun begins (well, I know freeVikings
+    # don't provide the real fun yet, but after a long startup procedure you
+    # could think about the simple game like about a fun).
+    # On the screen appears a strange place and three small vikings.
+    # Method 'game_loop' contains two nested loops.
+    # In the first one every iteration means one played location.
+    # A new iteration starts whenever a player finishes or gives up a location
+    # or if all the vikings are dead or if the remainder of them finishes the
+    # location.
+    # The nested loop updates all the sprites (heroes and their enemies)
+    # regularly and refreshes the screen.
+
     def game_loop
       loop do 
         paint_loading_screen @app_window
 
-	if @team.nil? then
+        if @team.nil? then
           level = location = nil
           level = @world.level
-	elsif (@team.alive_size < @team.size) or (@give_up == true) then
+        elsif (@team.alive_size < @team.size) or (@give_up == true) then
           if @give_up == true then
             @log.info "Game given up. Try once more."
           else
             @log.info "Some vikings died. Try once more."
           end
           level = @world.level
-	  @give_up = nil
-	elsif @team.alive_size == @team.size then
-	  # All of the vikings have reached the EXIT
-	  @log.info "Level completed."
-	  unless level = @world.next_level
+          @give_up = nil
+        elsif @team.alive_size == @team.size then
+          # All of the vikings have reached the EXIT
+          @log.info "Level completed."
+          unless level = @world.next_level
             p location
-	    @log.info "Congratulations! You explored all the world!"
+            @log.info "Congratulations! You explored all the world!"
             @state = AllLocationsFinishedGameState.new self
-	  end
-	else
-	  # Situation which shouldn't ever occur
-	  raise FatalError, '*** Really strange situation. Nor the game loop is in it\'s first loop, nor the level completed, no vikings dead. Send a bug report, please.'
-	end
+          end
+        else
+          # Situation which shouldn't ever occur
+          raise FatalError, '*** Really strange situation. Nor the game loop is in it\'s first loop, nor the level completed, no vikings dead. Send a bug report, please.'
+        end
 
         # This paragraph of code is important for a good work of
         # the text displayed at the end of the game. Be careful when
@@ -206,7 +179,7 @@ regularly and refreshes the screen.
         unless is_game_finished?
           location = @world.rewind_location 
           @team = init_vikings_team(location)
-
+          
           # Display (or not) password of the level
           if FreeVikings::OPTIONS["display_password"] then
             @state = LocationInfoGameState.new(self, level)
@@ -219,51 +192,51 @@ regularly and refreshes the screen.
 
         @bottompanel = BottomPanel.new @team
 
-	frames = 0 # auxiliary variable for fps computing
+        frames = 0 # auxiliary variable for fps computing
 
         if FreeVikings::OPTIONS['profile'] then
           Profiler__::start_profile
         end
 
         # The "event loop": serve events, update game state
-	while (not is_exit?) and (not @give_up) do
+        while (not is_exit?) and (not @give_up) do
 
-	  # Serve events:
-	  RUDL::EventQueue.get.each do |event|
-	    @state.serve_event(event, location)
-	  end
+          # Serve events:
+          RUDL::EventQueue.get.each do |event|
+            @state.serve_event(event, location)
+          end
 
           unless @team.active.alive?
             @team.next
           end
 
           # nearly all the game state (position of heroes etc.) is updated here
-	  location.update unless @state.paused?
-
-	  location.paint(@map_view, @team.active.center)
-	  @app_window.blit(@map_view, [0,0])
-
+          location.update unless @state.paused?
+          
+          location.paint(@map_view, @team.active.center)
+          @app_window.blit(@map_view, [0,0])
+          
           @state.change_view(@app_window)
 
-	  @app_window.blit(@bottompanel.image, [0, WIN_HEIGHT - BottomPanel::HEIGHT])
+          @app_window.blit(@bottompanel.image, [0, WIN_HEIGHT - BottomPanel::HEIGHT])
 
-	  if FreeVikings.display_fps? then
-	    @app_window.filled_polygon [[8,8],[60,8],[60,20],[8,20]], [0,0,0]
+          if FreeVikings.display_fps? then
+            @app_window.filled_polygon [[8,8],[60,8],[60,20],[8,20]], [0,0,0]
             unless (s = Time.now.sec) == 0 
               @app_window.print([10,10], "fps: #{frames / s}", 0xFFFFFFFF)
             else
               frames = 0
             end
-	  end
+          end
 
           if FreeVikings::OPTIONS['delay'] != 0 then
             sleep(FreeVikings::OPTIONS['delay'])
           end
 
-	  @app_window.flip
-	  frames += 1
+          @app_window.flip
+          frames += 1
 	  
-	end # while (not is_exit?) and (not @give_up)
+        end # while (not is_exit?) and (not @give_up)
 
         if FreeVikings::OPTIONS['profile'] then
           exit # the END block which prints out profiler output will be called
@@ -272,11 +245,8 @@ regularly and refreshes the screen.
       end # loop
     end # public method game_loop
 
-=begin
---- Game#run_location(location)
-This method is called from (({LocationInfoGameState})) to start playing.
-The vikings are put into the (({Location})) and fun starts.
-=end
+    # This method is called from LocationInfoGameState to start playing.
+    # The vikings are put into the Location and fun starts.
 
     def run_location(location)
       @team.each {|viking| 
@@ -290,6 +260,7 @@ The vikings are put into the (({Location})) and fun starts.
     # (or reloaded).
     # It recreates all the three vikings and sets them up
     # to start their way on the right place in the new loaded location.
+
     private
     def init_vikings_team(location)
       @baleog = Viking.createWarior("Baleog", location.start)
