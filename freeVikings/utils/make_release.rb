55583b447d098188aa57aedd3d77ff851d4b40ff
igneus_cz <igneus_cz>
2008-09-01 12:20:07 +0000

    auto-releasing tool finished.

--- freeVikings/utils/make_release.rb
+++ freeVikings/utils/make_release.rb
@@ -8,25 +8,26 @@
 
 CVS_ARGS = "-z3 -d:ext:igneus_cz@freevikings.cvs.sourceforge.net/cvsroot/freevikings"
 
-# Tasks:
-STEPS = [nil,
-         :make_branch, # - CVS: make branch
-         :create_directory, # - create directory in /tmp and chdir there
-         :checkout_new_branch, # - CVS: checkout new branch
-         :readme, # - modify README
-         :release, # - create RELEASE
-         :commit, # - CVS: commit
-         :remove_sandbox, # - remove the CVS working directory
-         :export, # - CVS: export
-         :rename, # - rename directory freeVikings to freeVikings-VERSION
-         :archives, # - create archive
-         :ftp, # - ftp archive to web
-         :freshmeat, :raa, # - update freshmeat and RAA entries
-         :cleanup # - delete tmp dir
-        ]
-
 # Most of the methods of this module are tasks of release process
 module Tasks
+
+  # Tasks:
+  STEPS = [nil,
+           :make_branch, # - CVS: make branch
+           :create_directory, # - create directory in /tmp and chdir there
+           :checkout_new_branch, # - CVS: checkout new branch
+           :readme, # - modify README
+           :release, # - create RELEASE
+           :commit, # - CVS: commit
+           :remove_sandbox, # - remove the CVS working directory
+           :export, # - CVS: export
+           :rename, # - rename directory freeVikings to freeVikings-VERSION
+           :archives, # - create archive
+           :ftp, # - ftp archive to web
+           :freshmeat, :raa, # - update freshmeat and RAA entries
+           :cleanup # - delete tmp dir
+          ]
+
 
   # not a task, helpful tool.
   # runs or puts CVS command.
@@ -168,25 +169,44 @@ module Tasks
     File.open("yafccommands", 'w') {|f| f.puts yafc_commands}
     system "cat yafccommands | #{yafc_invocation}"
   end
+
+  # freshmeat.net: release focus
+  RELEASE_FOCUS_CODES = ["N/A",
+                         "Initial freshmeat announcement",
+                         "Documentation",
+                         "Code cleanup",
+                         "Minor feature enhancements",
+                         "Major feature enhancements",
+                         "Minor bugfixes",
+                         "Major bugfixes",
+                         "Minor security fixes",
+                         "Major security fixes"]
+
+  # freshmeat.net XMLRPC interface: error codes
+  XMLRPC_ERRORCODES = {10 => "Login incorrect", 
+    20 => 'Session inconsistency',
+    21 => 'Session invalid', 30 => 'Branch ID incorrect',
+    40 => 'Permission to publish release denied',
+    50 => 'Version string missing', 51 => 'Duplicate version string',
+    60 => 'Changes field empty', 61 => 'Changes field too long',
+    62 => 'Changes field contains HTML', 70 => 'No valid email address set',
+    80 => 'Release not found', 81 => 'Project not found',
+    90 => 'Release focus missing', 91 => 'Release focus invalid',
+    100 => 'License invalid', 999 => 'Unknown error'}
      
   def freshmeat # - update freshmeat entry using freshmeat XML-RPC API
+    require 'uri'
     require 'xmlrpc/client'
-
-    error_codes = {10 => "Login incorrect", 20 => 'Session inconsistency',
-      21 => 'Session invalid', 30 => 'Branch ID incorrect',
-      40 => 'Permission to publish release denied',
-      50 => 'Version string missing', 51 => 'Duplicate version string',
-      60 => 'Changes field empty', 61 => 'Changes field too long',
-      62 => 'Changes field contains HTML', 70 => 'No valid email address set',
-      80 => 'Release not found', 81 => 'Project not found',
-      90 => 'Release focus missing', 91 => 'Release focus invalid',
-      100 => 'License invalid', 999 => 'Unknown error'}
 
     Dir.chdir $setup.tmpdir
 
+    print "Edit changes: "
     if ! File.exist? 'FRSHCHANGES' then
       system "cp freeVikings-#{$setup.dir_version}/RELEASE ./FRSHCHANGES"
       system "#{ENV['EDITOR']} FRSHCHANGES"
+      puts "changes file found; skipping"
+    else
+      puts "yes"
     end
 
     changes_text = ""
@@ -194,7 +214,16 @@ module Tasks
       f.each {|l| changes_text += " "+l.chomp}
     }
 
-    session = XMLRPC::Client.new "http://freshmeat.net/xmlrpc/"
+    puts "freshmeat: login"
+
+    # This trick with URI is from Freshmeat-Ruby by pabs:
+    # http://unix.freshmeat.net/projects/freshmeat-ruby/
+    # (I don't know exactly what it does, but it works,
+    # while when I had 
+    # session = XMLRPC::Client.new('http://freshmeat.net/xmlrpc/', '/RPC2', 80)
+    # it didn't...)
+    uri = URI::parse "http://freshmeat.net/xmlrpc"
+    session = XMLRPC::Client.new(uri.host, uri.request_uri, uri.port)
 
     print "Give freshmeat.net password: "
     password = gets.chomp
@@ -202,13 +231,15 @@ module Tasks
     loginpack = session.call("login", {'username' => 'igneus', 'password' => password})
     puts loginpack
     sid = loginpack['SID']
-    if loginpack['API Version'] != '1.02' then
+    if loginpack['API Version'] != '1.03' then
       raise "New API version - update!"
     end
 
+    puts "freshmeat: submit release"
+
     puts session.call("publish_release", {
                    'SID' => sid,
-                   'project_name' => freeVikings,
+                   'project_name' => 'freeVikings',
                    'branch_name' => 'Default',
                    'version' => $setup.version,
                    'changes' => changes_text,
@@ -216,16 +247,83 @@ module Tasks
                    'url_tgz' => 'http://freevikings.wz.cz/packages/'+$setup.ftp_dir_version+'/freeVikings-'+$setup.dir_version+".tar.gz"
                  })
 
+    puts "freshmeat: logout"
+
     puts session.call("logout", sid)
   end
 
   def raa # - update RAA entry
-    Dir.chdir $setup.tmpdir+'/freeVikings'
-    raise "not implemented" 
+    Dir.chdir $setup.tmpdir
+
+    # I haven't found any working remote-procedure-call interface for RAA
+    # updates, so I'll have to hack the web interface...
+
+    require 'net/http'
+    require 'uri'
+    require 'hpricot' # Why's HTML parsing library
+
+    # retrieve page with update form
+    uri = URI::parse 'http://raa.ruby-lang.org/update.rhtml?name=freevikings'
+    page = Net::HTTP.get uri
+
+    # get form data from it
+    doc = Hpricot(page)
+    form = (doc / "form").find {|f| f['action'] =~ /regist.rhtml/} # find the right form (there's another one for searching)
+
+    postdata = {}
+
+    inputs = form / "input"
+    inputs.each do |inp|
+      if ['submit', 'reset', 'password'].member? inp['type'] then
+        # useless inputs
+        next
+      elsif ! ['text','checkbox','hidden'].member? inp['type'] then
+        raise "Unsupported input type '#{inp['type']}'"
+      else
+        postdata[inp['name']] = inp['value']
+      end
+    end
+
+    selects = form / "select"
+    selects.each do |sel|
+      value = ''
+      others = []
+      (sel / "option").each do |opt|
+        if opt.has_attribute? "selected" then
+          value = opt['value']
+        end
+      end
+      if value == '' then
+        puts "Select #{sel['name']} has no default value - which to choose?"
+        others.each_with_index {|o,i| puts "#{i}: #{o}"}
+        i = gets.chom.to_i
+        value = others[i]
+      end
+      postdata[sel['name']] = value
+    end
+
+    textareas = form / "textarea"
+    textareas.each do |txt|
+      postdata[txt['name']] = txt.inner_html
+    end
+
+    # change what is needed
+
+    print "Type RAA password: "
+    password = gets.chomp
+
+    postdata['pass'] = password
+
+    postdata['minoredit'] = 'off'
+    postdata['version'] = $setup.version
+    postdata['download'] = 'http://freevikings.wz.cz/packages/'+$setup.ftp_dir_version+'/freeVikings-'+$setup.dir_version+".tar.gz"
+
+    # POST updated data to the server
+    puts Net::HTTP.post_form(URI::parse('http://raa.ruby-lang.org/regist.rhtml'),
+                             postdata)
   end
 
   def cleanup
-    Dir.chdir $setup.tmpdir+'/freeVikings'
     puts "Removing directory "+$setup.tmpdir
     Dir.rmdir $setup.tmpdir
   end
@@ -233,10 +331,10 @@ end
 
 # MAIN ===================================================================
 
-require 'getoptlong'
+require 'optparse'
 require 'ostruct'
 
-### get setup
+### set default setup
 
 $setup = OpenStruct.new
 $setup.step = 1
@@ -246,45 +344,69 @@ $setup.only_step = nil
 $setup.modules = ['freeVikings', 'gameui', 'schwerengine']
 $setup.release_focus = 4 # Minor feature enhancements
 
-options = GetoptLong.new(['--version', '-v', GetoptLong::REQUIRED_ARGUMENT], # release version number
-                         ['--pretend', '-p', GetoptLong::NO_ARGUMENT], # do not execute the commands, just write them out
-                         ['--step', '-s', GetoptLong::REQUIRED_ARGUMENT], # start with some step (work has been started before)
-                         ['--only-step', '-o', GetoptLong::REQUIRED_ARGUMENT], # do just one step
-                         ['--release-focus', '-f', GetoptLong::REQUIRED_ARGUMENT]) # freshmeat.net code for release focus:
-# 0 - N/A
-# 1 - Initial freshmeat announcement
-# 2 - Documentation
-# 3 - Code cleanup
-# 4 - Minor feature enhancements
-# 5 - Major feature enhancements
-# 6 - Minor bugfixes
-# 7 - Major bugfixes
-# 8 - Minor security fixes
-# 9 - Major security fixes
+### parse commandline options
 
+parser = OptionParser.new(ARGV) do |opts|
+  opts.separator "Options giving information about the release"
+  opts.separator ""
 
-options.each do |option, argument|
-  case option
-  when "--version"
-    $setup.version = argument
+  opts.on('-v', '--version VERSION',
+          "Version number/tag, e.g. '1.15' or '1.16pre7'") do |v|
+    $setup.version = v
     $setup.cvs_version = $setup.version.gsub(/[\s.]/, "-")
     $setup.dir_version = $setup.version.gsub(/\s/, '-')
     $setup.ftp_dir_version = $setup.version.sub(/\s/, "_")
-    $setup.tmpdir = '/tmp/fvrelease--'+$setup.ftp_dir_version+'-tmpdir'
-  when "--pretend"
-    $setup.pretend = true
-  when "--step"
-    $setup.step = argument.to_i
-  when "--only-step"
-    $setup.only_step = argument.to_i
-  when "--release-focus"
-    $setup.release_focus = argument
+    $setup.tmpdir = '/tmp/fvrelease--'+$setup.ftp_dir_version+'-tmpdir'    
+  end
+
+  opts.on('-f', '--release-focus', Integer,
+          "Set release focus for freshmeat.net; default: #{$setup.release_focus}") do |f|
+    $setup.release_focus = f
+  end
+
+  opts.separator ""
+  opts.separator "Options for choosing of subset of tasks to be done"
+  opts.separator ""
+
+  opts.on('-s', '--step STEP', Integer,
+          "Step (task) to start with") do |s|
+    $setup.step = s
+  end
+
+  opts.on('-o', '--only-step STEP', Integer,
+          "Do just one step (task)") do |s|
+    $setup.only_step = s
+  end
+
+  opts.separator ""
+  opts.separator "Other options"
+  opts.separator ""
+
+  opts.on('-p', '--pretend',
+          "Switches off some operations (mainly CVS transactions) and", 
+          "just prints what would be done.") do
+    $setup.pretend = true    
+  end
+
+  opts.on('-h', '--help',
+          "Print this help and exit") do
+    puts opts
+    puts
+    puts "Step numbers:"
+    puts
+    Tasks::STEPS.each_with_index {|s,i| puts "    #{i}: #{s}"}
+    puts
+    puts "Release focus codes"
+    puts
+    Tasks::RELEASE_FOCUS_CODES.each_with_index {|c,i| puts "    #{i}: #{c}"}  
+    exit    
   end
 end
+parser.parse!
 
+### check if compulsory arguments have been set
 if $setup.version.nil? then
-  STDERR.puts "Give version tag, please."
-  exit 1
+  raise "Version tag/number is a mandatory commandline argument."
 end
 
 ### check if we are in the freeVikings working directory and have also
