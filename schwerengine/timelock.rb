bcaafcd41c42654e7d71d16c7d90700e0a3cf416
igneus_cz <igneus_cz>
2008-10-31 23:17:52 +0000

    - two new methods for TimeLock (end_time and time_left)

--- schwerengine/timelock.rb
+++ schwerengine/timelock.rb
@@ -1,27 +1,18 @@
 # timelock.rb
 # igneus 22.7.2005
 
-=begin
-= NAME
-TimeLock
-
-= DESCRIPTION
-((<TimeLock>)) objects are used for a very frequent task. An instance is
-initialized by a number of seconds for which it must be locked.
-After this time it unlocks itself.
-=end
-
 module SchwerEngine
+
+  # TimeLock objects are used for a very frequent task. An instance is
+  # initialized by a number of seconds for which it must be locked.
+  # After this time it unlocks itself.
 
   class TimeLock
 
-=begin
---- TimeLock.new(seconds_to_expiry=0, timer=Time)
-The first argument is a number of seconds to the expiry time.
-The second one is an object which provides 'now' method to get the time
-information. Default is the standard Ruby built-in (({Time})) class,
-but you can use e.g. a (({SchwerEngine::Ticker})) instance as well.
-=end
+    # The first argument is a number of seconds to the expiry time.
+    # The second one is an object which provides 'now' method to get the time
+    # information. Default is the standard Ruby built-in Time class,
+    # but you can use e.g. a SchwerEngine::Ticker instance as well.
 
     def initialize(seconds_to_expiry=0, timer=Time)
       @timer = timer
@@ -29,17 +20,29 @@ but you can use e.g. a (({SchwerEngine::Ticker})) instance as well.
       @start_time = timer.now.to_i
     end
 
-=begin
---- TimeLock#free?
-Answers the question 'is the TimeLock free yet?'.
-=end
+    # Answers the question 'is the TimeLock free yet?'.
 
     def free?
-      if time_now >= @start_time + @seconds_to_expiry then
+      if time_now >= end_time then
         return true
       else
         return false
       end
+    end
+
+    # Returns Float saying how many seconds are left before the TimeLock
+    # is free
+
+    def time_left
+      return 0.0 if free?
+      return end_time - Time.now.to_f
+    end
+
+    # Returns Integer - end time (in seconds - value of timer.now.to_i 
+    # at that time)
+
+    def end_time
+      return @start_time + @seconds_to_expiry
     end
 
     private
